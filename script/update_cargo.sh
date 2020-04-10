09771a13c966b90267f57d4a765e98144ad2e6f8
Daniel Pfaff <daniel.pfaff@in.tum.de>
2020-04-10 16:29:22 +0200

    update_cargo.sh: Remove unused compile-time dependency tracking.

--- script/update_cargo.sh
+++ script/update_cargo.sh
@@ -1,8 +1,6 @@
 head -n $(grep -nF "[dependencies]" cargo/Cargo.toml | cut -f1 -d:) cargo/Cargo.toml > new
 mv new cargo/Cargo.toml
 
-rm cargo/rustc-deps
 for crate in $(rg -NIr "\$1" "^\\s*extern crate ([^;]+);" | sort -u); do
     cargo search $crate | head -n 1 >> cargo/Cargo.toml
-    printf " --extern $crate=cargo/target/{target}/release/deps/lib$crate-*.rlib" >> cargo/rustc-deps
 done
