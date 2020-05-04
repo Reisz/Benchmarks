15a1d87ec77c5eeb775713dec387866dc9d50500
Daniel Pfaff <daniel.pfaff@in.tum.de>
2020-05-04 11:52:57 +0200

    Bencher: Move diff code into separate header.

--- bencher/bencher.c
+++ bencher/bencher.c
@@ -1,9 +1,7 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <math.h>
 #include <time.h>
 #include <assert.h>
 #include <sched.h>
@@ -13,6 +11,7 @@
 #include <sys/resource.h>
 #include <sys/wait.h>
 
+#include "diff.h"
 #include "fileutils.h"
 #include "cpufreq.h"
 
@@ -28,129 +27,6 @@ struct Input {
 	size_t length;
 	char *text;
 };
-
-struct Diff {
-	size_t length;
-	char *text;
-	long double abserr;
-	char binary;
-};
-
-#define DIFF_COUNT 3
-int check_output(FILE *file, const struct Diff *diff) {
-	// Don't do anything when no diff is provided
-	if (!diff->text)
-		return 1;
-
-	// Special case for binary data (potentially containing \0)
-	if (diff->binary) {
-		int ok = 1;
-		size_t length;
-		char *comp = read_all_ptr(file, &length, 1, "Buffer");
-
-		if (length != diff->length) {
-			fprintf(stderr, "Error: Binary data lengths differ. (Expected %ld, got %ld)\n", diff->length, length);
-			ok = 0;
-		} else if (memcmp(diff->text, comp, length) != 0) {
-			fprintf(stderr, "Error: Binary data mismatch.\n");
-			ok = 0;
-		}
-
-		free(comp);
-		return ok;
-	}
-
-	// Variables for getline
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-
-	// Current position
-	char *diff_text = diff->text;
-	// Current Status
-	int ok = 1;
-	int error_count = 0;
-	while ((read = getline(&line, &len, file)) != -1) {
-		// Find diff line-end
-		char *diff_next = strchr(diff_text, '\n');
-
-		// Print error when diff ended before output
-		if (!diff_next) {
-			fprintf(stderr, "Error: Expected result ended before end of actual result.\n");
-			fprintf(stderr, "Remaining:\n%s", line);
-
-			int linecnt = 0;
-			while ((read = getline(&line, &len, file)) != -1) {
-				if (linecnt <= DIFF_COUNT)
-					fprintf(stderr, "%s", line);
-				++linecnt;
-			}
-
-			if (linecnt > DIFF_COUNT)
-				fprintf(stderr, "... %d more lines.\n", linecnt - DIFF_COUNT);
-
-			fprintf(stderr, "\n");
-
-			ok = 0;
-			break;
-		}
-		++diff_next; // include newline
-
-		// Calculate length of diff line
-		size_t diff_line = diff_next - diff_text;
-
-		// Textual diff first
-		// Unsing min is fine here because both strings include a terminating '\n'
-		#define MIN(a, b) (b) ^ (((a) ^ (b)) & -((a) < (b)))
-		int result = strncmp(line, diff_text, MIN(read, diff_line));
-
-
-		// Numerical diff as fallback
-		long double err = 0.0;
-		if (result != 0 && diff->abserr != 0) {
-			long double expected, actual;
-
-			int pos;
-			sscanf(diff_text, "%Lf%n", &expected, &pos);
-			sscanf(line, "%Lf", &actual);
-
-			err = fabsl(actual - expected);
-		}
-
-		// Print failure
-		if (result != 0 || err > diff->abserr) {
-			if (ok) {
-				fprintf(stderr, "Error: Diff failed.\n");
-				ok = 0;
-			}
-
-			if (error_count <= DIFF_COUNT) {
-				fprintf(stderr, "  Expected: %.*s", (int) diff_line, diff_text);
-				fprintf(stderr, "  Actual:   %s", line);
-				if (err != 0.0)
-					fprintf(stderr, "  Error:     %Lg\n", err);
-			}
-			++ error_count;
-		}
-
-		// Advance position of diff
-		diff_text = diff_next;
-	}
-
-	if (error_count > DIFF_COUNT)
-		fprintf(stderr, "... %d more errors.\n", error_count - DIFF_COUNT);
-
-	// Print error, when output ended before diff
-	if (diff_text != diff->text + diff->length) {
-		fprintf(stderr, "Error: Actual result ended before end of expected result.\n");
-		fprintf(stderr, "Remaining:\n%s\n", diff_text);
-
-		ok = 0;
-	}
-
-	free(line);
-	return ok;
-}
 
 #define CLOCK CLOCK_MONOTONIC
 #ifndef BUFFER
